0d2908b1dc9bdd6642fa9cda77cfd69b57c16ed1
GrittyGitty <58652339+GrittyGitty@users.noreply.github.com>
2022-05-06 14:01:24 +0300

    fixed mobile width issues of article - by giving w-full (width: 100%) *and* max-w-2xl to the article, it'll expand to the width of the container, or the width of max-w-2xl, whichever comes first

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div :dir="$dir()" class="bg-gray-100 w-full min-h-screen flex flex-col">
     <Header />
-    <div class="p-6 bg-white max-w-2xl mx-auto my-5 rounded flex-1">
+    <div class="p-6 bg-white w-full max-w-2xl mx-auto my-5 rounded flex-1">
       <Nuxt />
     </div>
     <Footer />
