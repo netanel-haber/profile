d8f3cdee2af3b56ec289c18915d66369bab210f7
GrittyGitty <58652339+GrittyGitty@users.noreply.github.com>
2022-05-11 19:23:48 +0300

    added clicky to footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,12 +1,23 @@
 <template>
   <footer class="border border-gray-200">
-    <div class="max-w-2xl mx-auto py-2">
+    <div class="max-w-2xl mx-auto p-2 text-center">
       {{ $t('written') }}
-      <a-out href="https://nuxtjs.org/">NuxtJS ❤️</a-out>.
+      <a-out href="https://nuxtjs.org/">NuxtJS ❤️</a-out>
+      <span class="mx-2">•</span>
       {{ $t('hosted') }}
-      <a-out href="https://pages.github.com/">Github Pages</a-out>. ({{
-        $t('because')
-      }})
+      <a-out href="https://pages.github.com/">Github Pages</a-out>
+      <span class="mx-2">•</span>
+      {{ $t('analytics_by') }}
+      <a
+        title="Privacy-friendly Web Analytics"
+        href="https://clicky.com/101366055"
+        ><img
+          class="inline"
+          alt="Clicky"
+          src="https://static.getclicky.com/media/links/badge.gif"
+          border="0"
+        />
+      </a>
     </div>
   </footer>
 </template>
@@ -15,9 +26,9 @@
 he:
   written: נכתב ב
   hosted: הוסטינג ב
-  because: רציתי לראות על מה כל המגניבים מדברים
+  analytics_by: אנליטיקות באמצעות
 en:
   written: Written in
   hosted: hosted on
-  because: I wanted to see what all the cool kids are talking about
+  analytics_by: Analytics by
 </i18n>
