91d70bbcc9ccc8942403bbd3a553a6520ceed298
Netanel Haber <netanel.t.haber@gmail.com>
2024-02-25 23:57:48 +0200

    under engineering

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,25 +1,35 @@
 <template>
   <div class="mx-auto flex flex-col gap-2">
     <span class="p-2 border bg-neutral-100 text-neutral-600 mb-7">
-      My name is Netanel Haber, I do ML/MLOps/FullStack/DevOps yada yada, I work here: <a-out
-        href=" https://deci.ai/">Deci AI</a-out>
+      My name is Netanel Haber, I do ML/MLOps/FullStack/DevOps yada yada, I work here: <a-out class="text-blue-600"
+        href="https://deci.ai/">Deci AI</a-out>
     </span>
-    <div class="flex flex-wrap gap-4">
+    <hr />
+    <section class="flex flex-wrap gap-4 p-2">
       <a-out href="https://netanel-haber.github.io/checkers/"
-        class="flex-1 text-white p-4 text-center bg-black bg-no-repeat bg-center bg-contain w-16">Vanilla TS
-        Checkers
-        <img src="/checkers.png" />
+        style="background-image: linear-gradient(rgba(115, 104, 122, 0.709), rgb(93, 20, 133))"
+        class="flex flex-1 flex-col w-30 rounded-md p-1 justify-between shadow-inner">
+        <span class="text-white mb-2 text-center">Vanilla TS Checkers</span>
+        <img style="box-shadow: inset 0 0 10px rgba(0, 0, 0, 0.5);" src="/checkers.png" class="w-full h-auto p-2" />
       </a-out>
       <a-out href="https://netanel-haber.github.io/chatgpt-custom-instructions/"
-        class="flex-1 text-white p-4 text-center bg-black bg-no-repeat bg-center bg-contain w-16">My ChatGPT Custom
-        Instructions
-        <img src="https://upload.wikimedia.org/wikipedia/commons/0/04/ChatGPT_logo.svg" /></a-out>
-      <a-out href="https://netanel.dev/pwd"
-        class="flex-1 text-white p-4 text-center bg-black bg-no-repeat bg-center bg-contain w-16">Chrome Suggest
-        Password
-        <img src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Google_Chrome_icon_%28February_2022%29.svg" />
+        style="background-image: radial-gradient(circle, rgb(216, 200, 200) 0%, #00a67fae 100%)"
+        class="flex flex-1 flex-col w-30 rounded-md p-1 justify-between shadow-inner">
+        <span class="text-white mb-2 text-center">My ChatGPT Custom Instructions</span>
+        <img style="box-shadow: inset 0 0 10px rgba(0, 0, 0, 0.5);"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/04/ChatGPT_logo.svg" class="w-full h-auto p-2" />
       </a-out>
-    </div>
+      <a-out href="https://netanel.dev/pwd"
+        style="background-image: radial-gradient(circle at 20% 4%, #4A90E2 0%, #4A90E2 25%, #DB4437 50%, #DB4437 75%, #F4B400 100%), radial-gradient(circle at 4% 4%, #4A90E2 0%, #4A90E2 25%, #0F9D58 50%, #F4B400 75%, #F4B400 100%);"
+        class="flex flex-1 flex-col w-30 rounded-md p-1 justify-between shadow-inner">
+        <span class="text-white mb-2 text-center">Chrome Strong Password Generator</span>
+        <img style="box-shadow: inset 0 0 10px rgba(0, 0, 0, 0.5);"
+          src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Google_Chrome_icon_%28February_2022%29.svg"
+          class="w-full h-auto p-2" />
+      </a-out>
+    </section>
+    <hr />
+    <!-- <iframe height="100vh" src="https://netanel-haber.github.io/checkers/"></iframe> -->
     <section class="flex flex-col gap-10">
       <article v-for="(post, $index) in posts" :key="`post-${$index}`">
         <div class="p-1">
@@ -67,6 +77,5 @@ export default {
       }
     ]
   })
-
 }
 </script>
