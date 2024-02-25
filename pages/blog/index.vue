cdd22d462b578714b4be5dd6e8dc6f0a47e876e7
Netanel Haber <netanel.t.haber@gmail.com>
2024-02-25 22:38:59 +0200

    under engineering

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -4,6 +4,22 @@
       My name is Netanel Haber, I do ML/MLOps/FullStack/DevOps yada yada, I work here: <a-out
         href=" https://deci.ai/">Deci AI</a-out>
     </span>
+    <div class="flex flex-wrap gap-4">
+      <a-out href="https://netanel-haber.github.io/checkers/"
+        class="flex-1 text-white p-4 text-center bg-black bg-no-repeat bg-center bg-contain w-16">Vanilla TS
+        Checkers
+        <img src="/checkers.png" />
+      </a-out>
+      <a-out href="https://netanel-haber.github.io/chatgpt-custom-instructions/"
+        class="flex-1 text-white p-4 text-center bg-black bg-no-repeat bg-center bg-contain w-16">My ChatGPT Custom
+        Instructions
+        <img src="https://upload.wikimedia.org/wikipedia/commons/0/04/ChatGPT_logo.svg" /></a-out>
+      <a-out href="https://netanel.dev/pwd"
+        class="flex-1 text-white p-4 text-center bg-black bg-no-repeat bg-center bg-contain w-16">Chrome Suggest
+        Password
+        <img src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Google_Chrome_icon_%28February_2022%29.svg" />
+      </a-out>
+    </div>
     <section class="flex flex-col gap-10">
       <article v-for="(post, $index) in posts" :key="`post-${$index}`">
         <div class="p-1">
