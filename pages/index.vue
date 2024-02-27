fd83039df2530d51da6e69497b6cfbba6aca1bdb
Netanel Haber <netanel.t.haber@gmail.com>
2024-02-27 19:52:24 +0200

    less complex

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="mx-auto flex flex-col">
+    <section class="mb-3 flex flex-col gap-3">
+      <article>
+        <div class="p-1">
+          <div class="mb-2">
+            <nuxt-link class="text-blue-600 text-lg" to="/concurrency">
+              Concurrency
+            </nuxt-link>
+          </div>
+          <div class="text-sm text-gray-600">Yes, Python has threads (*Sigh*)</div>
+        </div>
+        <hr />
+      </article>
+      <article>
+        <div class="p-1">
+          <div class="mb-2">
+            <nuxt-link class="text-blue-600 text-lg" to="/typescript-typesystems-and-javascript">
+              TypeScript, TypeSystems and JavaScript
+            </nuxt-link>
+          </div>
+          <div class="text-sm text-gray-600">Notepad -> Notepad++</div>
+        </div>
+        <hr />
+      </article>
+    </section>
+    <apps />
+  </div>
+</template>
