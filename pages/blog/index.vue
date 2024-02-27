71ecdfce2badfa88124af7b4cdff7f4e6219f387
Netanel Haber <netanel.t.haber@gmail.com>
2024-02-27 19:20:03 +0200

    less complex

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,22 +1,20 @@
 <template>
   <div class="mx-auto flex flex-col">
     <section class="mb-3 flex flex-col gap-3">
-      <article v-for="(post, $index) in posts" :key="`post-${$index}`">
-        <div class="p-1">
-          <div class="mb-2">
-            <nuxt-link class="text-blue-600 text-lg" :to="post._path">
-              {{ post.title }}
-            </nuxt-link>
+      <ContentList path="/blog" v-slot="{ list }">
+        <article v-for="post in list" :key="post._path">
+          <div class="p-1">
+            <div class="mb-2">
+              <nuxt-link class="text-blue-600 text-lg" :to="post._path">
+                {{ post.title }}
+              </nuxt-link>
+            </div>
+            <div class="text-sm text-gray-600">{{ post.description }}</div>
           </div>
-          <div class="text-sm text-gray-600">{{ post.description }}</div>
-        </div>
-        <hr />
-      </article>
+          <hr />
+        </article>
+      </ContentList>
     </section>
     <apps />
   </div>
 </template>
-
-<script setup>
-const posts = await queryContent("/blog").find()
-</script>
