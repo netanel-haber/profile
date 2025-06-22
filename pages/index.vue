22ac0e49fbea719ffed54c5647827500aa95b8d6
Netanel Haber <58652339+netanel-haber@users.noreply.github.com>
2025-06-22 14:05:55 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,5 @@
 <template>
   <main class="mx-auto flex flex-col">
     <apps />
-    <section class="mb-3 flex flex-col gap-3">
-      <article v-for="article in articles">
-        <div class="p-1">
-          <div class="mb-2">
-            <nuxt-link class="text-blue-600 text-lg" :to="article.to">
-              {{ article.title }}
-            </nuxt-link>
-          </div>
-          <div class="text-sm text-gray-600">{{ article.description }}</div>
-        </div>
-        <hr />
-      </article>
-    </section>
   </main>
 </template>
-
-<script setup lang="ts">
-const articles = [
-  { title: "Ok, Sometimes Async-Await", description: "I'll concede in 3 cases", to: "/ok-sometimes-async-await" },
-  { title: "Concurrency", description: "Yes, Python has threads (*Sigh*)", to: "/concurrency" },
-  { title: "TypeScript, TypeSystems and JavaScript", description: "Notepad -> Notepad++", to: "/typescript-typesystems-and-javascript" }
-]
-</script>
