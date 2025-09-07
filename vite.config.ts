e78da798cd94b81f196d21b7e5651d9eaba7e94d
Netanel Haber <netanel.t.haber@gmail.com>
2025-09-07 11:30:36 +0300

    preserve blog
    Signed-off-by: Netanel Haber <netanel.t.haber@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+const pages = ["index.html", "concurrency.html", "typescript-typesystems-and-javascript.html", "ok-sometimes-async-await.html"]
 
 export default defineConfig({
     plugins: [
         tailwindcss(),
     ],
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                ...Object.fromEntries(pages.map(page => [page, resolve(__dirname, page)])),
+            },
+        },
+    },
 });
